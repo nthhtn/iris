174e4626be152d0663943e6bcc11633e350fe13e
Hien Nguyen <thehien115@gmail.com>
2017-06-06 16:42:40 +0700

    Added configuration infrastructure

--- bin/run.js
+++ bin/run.js
@@ -1,15 +1,16 @@
 'use strict';
 
+const config = require('../config/index');
 const service = require('../server/service');
 const http = require('http');
 const server = http.createServer(service);
 const serviceRegistry = service.get('serviceRegistry');
 
-const witToken = 'CJW5DXMEUZ2UNMPHI63FI3ZYJ4VVZ5FK';
+const witToken = config.witToken;
 const witClient = require('../server/witClient')(witToken);
 
 const slackClient = require('../server/slackClient');
-const slackToken = 'xoxb-190304540482-NNNU4bJjPSwWLeyS8aV3INjX';
+const slackToken = config.slackToken;
 const slackLogLevel = 'verbose';
 const rtm = slackClient.init(slackToken, slackLogLevel, witClient, serviceRegistry);
 rtm.start();
